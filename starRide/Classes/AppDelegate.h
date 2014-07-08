e985283f27c7ff06aa3a69f47e9a5f16942f58f7
Sebastien Villar <me@sebastienvillar.com>
2014-07-08 21:12:40 +0200

    Initial Commit

---
+++ starRide/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  starRide
+//
+//  Created by Sebastien Villar on 08/07/14.
+//  Copyright VillarGames 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
